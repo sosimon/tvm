7832826d5f8d0385dbf170a8942a6e04df94ca0f
Simon So <simon.so@gmail.com>
2017-05-12 10:47:05 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Token Vending Machine
+
+The vending machine generates temporary credentials to your AWS account - access and secret keys for APIs and a URL for the AWS console. The generated temporary credentials has access permissions that is the _intersection_ of
+
+    1. The policy that is passed into the `GetFederationToken` call, and
+    2. Policies that are attached to the IAM user whose credentials were used to all `GetFederationToken`.
+
+See [docs](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_temp_control-access_getfederationtoken.html) for more details. Currently, the policy passed into `GetFederationToken` is hardcoded to allow full access to EC2 and to manage SSH key pairs.
+
+The temporary credentials are defaulted to expire after 15 minutes (900 seconds), which is the minimum session duration. A longer expiration can be specified, in seconds, using the `-x` flag, up to 36 hours (129600 seconds).
+
+## Setup
+
+Setup AWS credentials the same way you would for AWS CLI. See [SDK Configuration](http://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html#id2) and [AWS CLI Configuration](http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html#config-settings-and-precedence) for more information.
+
+## Usage
+
+```
+tvm [-u tempUsername] [-x sessionDuration]
+```
+
+## Example
+
+```
+tvm -u foobar -x 3600
+```
+
+## References
+
+[Creating a URL that Enables Federated Users to Access the AWS Management Console](http://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_providers_enable-console-custom-url.html)
---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/fatih/color"
+)
+
+// Credentials struct holds the session details obtained from the response
+// of GetFederationToken call
+type Credentials struct {
+	AccessKeyId     string `json:"sessionId"`
+	SecretAccessKey string `json:"sessionKey"`
+	SessionToken    string `json:"sessionToken"`
+}
+
+// SigninTokenResp holds the response from token request call to AWS
+type SigninTokenResp struct {
+	SigninToken string `json:"SigninToken"`
+}
+
+func main() {
+	user := flag.String("u", "test-user", "Name of temporary user")
+	expiry := flag.Int64("x", 900, "Credentials expiration time in seconds")
+	flag.Parse()
+
+	// Create Session
+	sess := session.Must(session.NewSession())
+
+	// Create STS service
+	svc := sts.New(sess, aws.NewConfig().WithRegion("us-west-2"))
+
+	// Create params for GetFederationToken()
+	params := &sts.GetFederationTokenInput{
+		DurationSeconds: aws.Int64(*expiry),
+		Name:            aws.String(*user),
+		Policy:          aws.String(`{"Version": "2012-10-17","Statement": [{"Action": "ec2:*","Effect": "Allow","Resource": "*"},{"Effect": "Allow","Action": ["iam:GetSSHPublicKey","iam:ListSSHPublicKeys","iam:UpdateSSHPublicKey","iam:UploadSSHPublicKey"],"Resource": ["*"]}]}`),
+	}
+	resp, err := svc.GetFederationToken(params)
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println(err.Error())
+		return
+	}
+
+	expiration := *resp.Credentials.Expiration
+
+	// Create a Credentials struct from response and serialize
+	sessionString, err := json.Marshal(Credentials{
+		AccessKeyId:     *resp.Credentials.AccessKeyId,
+		SecretAccessKey: *resp.Credentials.SecretAccessKey,
+		SessionToken:    *resp.Credentials.SessionToken,
+	})
+	if err != nil {
+		log.Fatalf("JSON marshal failed: %s", err)
+	}
+
+	// Build the federated token request URL
+	tokenReqURL := buildTokenReqURL(string(sessionString))
+
+	// Send the request to get an AWS console signin token
+	signinToken := reqSigninToken(tokenReqURL.String())
+
+	// Build the AWS console signin URL
+	loginURL := buildLoginURL(signinToken)
+
+	// Print credentials and console signin URL
+	c := color.New(color.FgGreen).Add(color.Bold)
+	c.Printf("Access Key: ")
+	fmt.Printf("%s\n", *resp.Credentials.AccessKeyId)
+	c.Printf("Secret Key: ")
+	fmt.Printf("%s\n", *resp.Credentials.SecretAccessKey)
+	c.Printf("Session Token: ")
+	fmt.Printf("%s\n", *resp.Credentials.SessionToken)
+	c.Printf("URL: ")
+	fmt.Printf("%s\n", loginURL)
+	c.Printf("Credentials expires at: ")
+	fmt.Printf("%s\n", expiration.Local().Format(time.UnixDate))
+}
+
+func reqSigninToken(reqURL string) string {
+	var str SigninTokenResp
+	res, err := http.Get(reqURL)
+	if err != nil {
+		log.Fatalf("HTTP request failed: %s", err)
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body: %s", err)
+	}
+	err = json.Unmarshal(body, &str)
+	if err != nil {
+		log.Fatalf("JSON unmarshal failed: %s", err)
+	}
+	return str.SigninToken
+}
+
+func buildTokenReqURL(sessionString string) *url.URL {
+	v := url.Values{}
+	v.Add("Action", "getSigninToken")
+	v.Add("Session", sessionString)
+	return &url.URL{
+		Scheme:   "https",
+		Host:     "signin.aws.amazon.com",
+		Path:     "federation",
+		RawQuery: v.Encode(),
+	}
+}
+
+func buildLoginURL(signinToken string) *url.URL {
+	v := url.Values{}
+	v.Add("Action", "login")
+	v.Add("Destination", "https://console.aws.amazon.com/")
+	v.Add("SigninToken", signinToken)
+	return &url.URL{
+		Scheme:   "https",
+		Host:     "signin.aws.amazon.com",
+		Path:     "federation",
+		RawQuery: v.Encode(),
+	}
+}
